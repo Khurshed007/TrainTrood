f717953f6e3cbfe22b209fea1aa4b0a731ec95e7
Khurshed007 <khurshed.ahmadjonov@bk.ru>
2024-11-20 17:31:23 +0100

    lalala

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Input } from './shared/ui/input';
 function App() {
   return (
     <div className="flex items-center justify-center w-1/2 h-screen mx-auto border border-gray-300">
+      <div className="text-red-500">Hello, Tailwind!</div>
       <div className="flex flex-col items-center justify-center w-full max-w-sm p-4 bg-white border border-gray-200 shadow-md rounded">
         <Input placeholder="asdasd" />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-
-/** 
- * @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
       center: true,
@@ -46,41 +44,12 @@ module.exports = {
         ring: 'hsl(var(--ring))'
       }
     },
+    borderRadius: {
+      sm: 'calc(var(--radius) - 4px)',
+      md: 'calc(var(--radius) - 2px)',
+      lg: 'var(--radius)',
+      xl: 'calc(var(--radius) + 4px)'
+    }
   },
-  plugins: [require("tailwindcss-animate")],
-}
-// colors: {
-//   border: "hsl(var(--border))",
-//   input: "hsl(var(--input))",
-//   ring: "hsl(var(--ring))",
-//   background: "hsl(var(--background))",
-//   foreground: "hsl(var(--foreground))",
-//   primary: {
-//     DEFAULT: "hsl(var(--primary))",
-//     foreground: "hsl(var(--primary-foreground))",
-//   },
-//   secondary: {
-//     DEFAULT: "hsl(var(--secondary))",
-//     foreground: "hsl(var(--secondary-foreground))",
-//   },
-//   destructive: {
-//     DEFAULT: "hsl(var(--destructive))",
-//     foreground: "hsl(var(--destructive-foreground))",
-//   },
-//   muted: {
-//     DEFAULT: "hsl(var(--muted))",
-//     foreground: "hsl(var(--muted-foreground))",
-//   },
-//   accent: {
-//     DEFAULT: "hsl(var(--accent))",
-//     foreground: "hsl(var(--accent-foreground))",
-//   },
-//   popover: {
-//     DEFAULT: "hsl(var(--popover))",
-//     foreground: "hsl(var(--popover-foreground))",
-//   },
-//   card: {
-//     DEFAULT: "hsl(var(--card))",
-//     foreground: "hsl(var(--card-foreground))",
-//   },
-// },
+  plugins: []
+};
